3e8213eb7987623fbb9d660a7b23b016aeb439c2
r1cc4rdo <riccardo.gherardi@gmail.com>
2024-03-20 15:14:32 +1100

    Initial commit

---
+++ ptvv3.py
@@ -0,0 +1,41 @@
+import requests
+import hashlib
+import hmac
+
+
+class PTVv3:
+    """
+    Minimal implementation of the Public Transport Victoria (PTV) v3 API.
+    For documentation and instructions to obtain an id/key pair please visit:
+    https://www.ptv.vic.gov.au/footer/data-and-reporting/datasets/ptv-timetable-api
+    """
+    
+    base_url = 'https://timetableapi.ptv.vic.gov.au'
+
+    def __init__(self, ptv_id, ptv_key, debug=False):
+        
+        self.id = ptv_id
+        self.key = ptv_key.encode('utf-8')
+        self.debug = debug
+
+    def __call__(self, endpoint, **params):
+        """
+        Signs and performs a request; throws on failure.
+        Returns the JSON encoded response.
+        """
+        params['devid'] = self.id
+        request = f'{endpoint}?{"&".join(f"{k}={v}" for k, v in params.items())}'
+        hashed = hmac.new(self.key, request.encode('utf-8'), hashlib.sha1)
+        url = f'{PTVv3.base_url}{request}&signature={hashed.hexdigest()}'
+        if self.debug:
+            print(url)
+        
+        response = requests.get(url)
+        assert response.status_code == 200
+        return response.json()
+
+
+if __name__ == '__main__':
+    ptv_id, ptv_key = 'your id here', 'your key here'
+    ptv = PTVv3(ptv_id, ptv_key, debug=True)
+    print(ptv('/v3/disruptions', route_types=2))
